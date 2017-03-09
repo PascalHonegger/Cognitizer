c49971ff4c6e07c0f89e7b02a20c9d9ba549b6b2
Seraphin Rihm <serafin717@gmail.com>
2017-03-09 15:59:14 +0100

    better swag views code is made from the hands of god :p

--- app/src/main/java/com/example/informatik/cognitizer/AnalyseActivity.java
+++ app/src/main/java/com/example/informatik/cognitizer/AnalyseActivity.java
@@ -1,29 +1,44 @@
 package com.example.informatik.cognitizer;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class AnalyseActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analyse);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                        switch (item.getItemId()) {
+                            case R.id.action_text:
+                                //TODO: manager.beginTransaction().replace(R.id.analysePlaceholder, new LoginFragment()).commit();
+
+                            case R.id.action_picture:
+                                //TODO: manager.beginTransaction().replace(R.id.analysePlaceholder, new RegisterFragment()).commit();
+
+                        }
+                        return true;
+                    }
+                });
+
     }
 
 }
